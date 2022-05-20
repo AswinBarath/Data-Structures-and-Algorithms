61c3f6ea48f9866d41caec940ea3297825d028be
ashwin <aswin2001barath@gmail.com>
2022-05-20 17:05:46 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (46.59%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        // edge case
+        if(num == 1)
+            return true;
+        
+        // Take long to also include decimal test cases
+        long lo = 1;
+        // Reducing search space based on given number
+        long hi = num / 2;
+        
+        while(lo<=hi) {
+            long mid = lo + (hi - lo) / 2;
+            long square = mid * mid;
+            
+            if(square > num) {
+                hi = mid - 1;
+            }
+            else if(square < num) {
+                lo = mid + 1;
+            } 
+            else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
