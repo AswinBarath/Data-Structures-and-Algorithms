def5855e6112327db6eec89e2bdb050fb813d8c6
ashwin <aswin2001barath@gmail.com>
2022-05-20 16:09:15 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (34.40%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,23 +1,13 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-		int[][] dp = new int[m][n];
-		for (int[] row : dp)
-			Arrays.fill(row, -1);
-		return countPaths(0, 0, m, n, dp);
-	}
-
-	public int countPaths(int i, int j, int m, int n, int[][] dp) {
-		// Base cases
-		if (i == (m - 1) && j == (n - 1))
-			return 1;
-		if (i >= m || j >= n)
-			return 0;
-
-		// Memoization
-		if (dp[i][j] != -1)
-			return dp[i][j];
-
-		// Recursive cases
-		return dp[i][j] = countPaths(i + 1, j, m, n, dp) + countPaths(i, j + 1, m, n, dp);
-	}
+        int N = n + m - 2;
+		int r = (m-1) > (n-1) ? (n-1) : (m-1);
+		double res = 1;
+		
+		for(int i=1; i <= r; i++) {
+			res = res * (N - r + i) / i;
+		}
+		
+		return (int) res;
+    }
 }
