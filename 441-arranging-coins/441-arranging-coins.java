6b4b631fff582516ab95e6c381e90a47e41b784e
ashwin <aswin2001barath@gmail.com>
2022-05-22 12:47:57 +0530

    Time: 2 ms (90.73%), Space: 41.5 MB (47.68%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.java
+++ 441-arranging-coins/441-arranging-coins.java
@@ -1,19 +1,6 @@
 class Solution {
-    public int arrangeCoins(int num) {
-        // TODO Determine no. of coins required to arrange complete staircase using
-		// binary search
-		long left = 1, right = num;
-		long result = 0;
-		while (left <= right) {
-			long mid = left + (right - left) / 2;
-			long coins = (mid * (mid + 1)) / 2; // Gauss's formula
-			if (coins > num) {
-				right = mid - 1;
-			} else {
-				left = mid + 1;
-				result = Math.max(result, mid);
-			}
-		}
-		return (int) result;
-    }
+  public int arrangeCoins(int num) {
+	int k = (int)(Math.sqrt(2 * (long)num + 0.25) - 0.5);
+    return k;
+  }
 }
