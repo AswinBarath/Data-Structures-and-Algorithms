9bc58af55751af33d25997aea79d2cb046c6f03c
ashwin <aswin2001barath@gmail.com>
2022-05-23 09:27:12 +0530

    Time: 64 ms (54.19%), Space: 42.3 MB (80.59%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -1,36 +1,21 @@
 class Solution {
-    int[][][] dp;
+	int[][] dp;
 
 	public int findMaxForm(String[] strs, int m, int n) {
-		dp = new int[m + 1][n + 1][strs.length];
-		return helper(strs, m, n, 0);
-	}
-
-	public int helper(String[] strs, int zero, int one, int index) {
-		if (index == strs.length || zero + one == 0) {
-			return 0;
-		}
-
-		if (dp[zero][one][index] > 0) {
-			return dp[zero][one][index];
-		}
-
-		int[] count = count(strs[index]);
-
-		// consider - changes to zeros and ones
+		dp = new int[m + 1][n + 1];
 		int consider = 0;
-
-		if (zero >= count[0] && one >= count[1]) {
-			consider = 1 + helper(strs, zero - count[0], one - count[1], index + 1);
+		int skip = 0;
+		for (String s : strs) {
+			int[] count = count(s);
+			for (int zero = m; zero >= count[0]; zero--) {
+				for (int one = n; one >= count[1]; one--) {
+					consider = 1 + dp[zero - count[0]][one - count[1]];
+					skip = dp[zero][one];
+					dp[zero][one] = Math.max(consider, skip);
+				}
+			}
 		}
-
-		// skip - changes to zeros and ones
-		int skip = helper(strs, zero, one, index + 1);
-
-		// Memoization
-		dp[zero][one][index] = Math.max(consider, skip);
-		
-		return dp[zero][one][index];
+		return dp[m][n];
 	}
 
 	public int[] count(String s) {
