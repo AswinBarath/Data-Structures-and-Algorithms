9e2c146d4abec31df0107e030c0d6de7c217cfd4
ashwin <aswin2001barath@gmail.com>
2022-05-24 14:45:41 +0530

    Time: 3 ms (47.57%), Space: 41.7 MB (64.45%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,18 +1,17 @@
 class Solution {
     public boolean isValid(String s) {
         // TODO Check if given string is a valid parentheses
-        Stack<Character> stack = new Stack<>();
+		Stack<Character> stack = new Stack<>();
 		for (char c : s.toCharArray()) {
 			if (c == '(' || c == '[' || c == '{') {
 				stack.push(c);
 			} else {
-				if (stack.isEmpty()) {
+				if (stack.isEmpty())
 					return false;
-				}
 				char curr = stack.pop();
 				if ((curr == '(' && c == ')') 
-                    || (curr == '[' && c == ']') 
-                    || (curr == '{' && c == '}'))
+						|| (curr == '[' && c == ']') 
+						|| (curr == '{' && c == '}'))
 					continue;
 				else
 					return false;
