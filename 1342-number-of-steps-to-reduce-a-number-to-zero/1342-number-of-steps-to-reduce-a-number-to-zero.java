a9e31053be8625210653bd5aba946c9246a5cd7b
ashwin <aswin2001barath@gmail.com>
2022-05-27 10:21:07 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (97.76%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,7 +1,5 @@
 class Solution {
     public int numberOfSteps (int num) {
-        System.out.println(Integer.toBinaryString(num));
-        System.out.println(Integer.bitCount(num));
         int maxBinaryDigits = Integer.toBinaryString(num).length() - 1;
         int numOfOnes = Integer.bitCount(num);
 	    return maxBinaryDigits + numOfOnes;
