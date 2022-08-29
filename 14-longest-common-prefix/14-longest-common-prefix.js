60aa12ea16bb255f91e41ea7aa6cabfab0e155ba
ashwin <aswin2001barath@gmail.com>
2022-08-29 19:50:06 +0530

    Time: 64 ms (96.51%), Space: 41.8 MB (97.61%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -5,9 +5,11 @@
 var longestCommonPrefix = function(strs) {
     strs.sort();
     let count = 0;
+    let first = strs[0];
+    let last = strs[strs.length - 1];
     for(let i=0; i<Math.min(strs[0].length, strs[strs.length - 1].length); i++) {
-        if(strs[0][i] === strs[strs.length - 1][i]) count++;
+        if(first[i] === last[i]) count++;
         else break;
     }
-    return strs[0].substring(0, count);
+    return first.substring(0, count);
 };
